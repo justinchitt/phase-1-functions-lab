cb277b8102223750253efe5948b569d7e287ce48
Justin Chittarath <justinchitt@gmail.com>
2021-09-20 21:31:16 -0600

    commit tree 8a56c63b89481049da66eaddd9c716a63ed3a18d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(42 - blocks);
+}
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264;
+}
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((end - start) * 264);
+}
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264);
+  }
+  
+  function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    if (distance < 400) {
+      return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+      return ((distance - 400) * .02);
+    }
+    else if ((distance >= 2000) && (distance <= 2500)) {
+      return 25;
+    }
+    else {
+      return 'cannot travel that far';
+    }
+  }
